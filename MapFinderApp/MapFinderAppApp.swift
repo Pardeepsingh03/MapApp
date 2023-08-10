277093d261adef30734aa36f714443593c0bd3e8
Pardeepsingh03 <ps454075@gmail.com>
2023-08-10 12:09:28 +0530

    Initial Commit

---
+++ MapFinderApp/MapFinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapFinderAppApp.swift
+//  MapFinderApp
+//
+//  Created by MBA-0019 on 10/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MapFinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
